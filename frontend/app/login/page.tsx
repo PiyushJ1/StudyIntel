e422717f1e3858615cb4e73ff93748dce6f8157b
Piyush <piyxsh@gmail.com>
2025-06-20 02:44:28 +1000

    create login form and redirect to it

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,3 +1,14 @@
-export default function DashboardPage() {
-    return <div>Coming soon</div>;
+import styles from './login.module.css'
+
+export default function LoginPage() {
+    return (
+        <>
+        <form>
+            <div className={styles.login}>
+                <input type='text' name='username' placeholder='Enter your username' required></input>
+                <input type='text' name='username' placeholder='Enter your username' required></input>
+            </div>
+        </form>
+        </>
+    );
 }
