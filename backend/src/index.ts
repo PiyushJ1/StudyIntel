ce10079542d0a5e5352c7674b4029063b13c5cc9
Piyush <piyxsh@gmail.com>
2025-06-27 17:14:41 +1000

    use router for registering new user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import waitlistRoutes from "./routes/waitlistRoutes";
+import registerUserRoutes from "./routes/registerUserRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,7 +13,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Backend running successfully.");
 });
 
+// save email to waitlist
 app.use("/api/waitlist", waitlistRoutes);
+
+// register new user and save their info
+app.use("/api/register", registerUserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
