705645b245dcbdb9d89323ed3b8e99bfe49c79d0
Piyush <piyxsh@gmail.com>
2025-06-27 17:03:36 +1000

    config lint to work

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,26 +1,26 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import tsPluginPkg from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import { defineConfig } from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import tsPluginPkg from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
 
 const { configs: tsConfigs } = tsPluginPkg;
 
 export default defineConfig([
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: {
       parser: tsParser,
       globals: { ...globals.node },
     },
     plugins: {
-      '@typescript-eslint': tsPluginPkg,
+      "@typescript-eslint": tsPluginPkg,
     },
     rules: tsConfigs.recommended.rules,
   },
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ['js/recommended'],
+    extends: ["js/recommended"],
   },
 ]);
