978d00717ffe1ac268eeab4d7c92466ce9d7e082
Piyush <piyxsh@gmail.com>
2025-06-30 01:18:50 +1000

    hash new user passwords + add error checking for already registered email

--- backend/src/utils/registerUserStorage.ts
+++ backend/src/utils/registerUserStorage.ts
@@ -1,17 +1,12 @@
 import fs from "fs";
 import path from "path";
+import bcrypt from 'bcrypt';
 import { User } from "../models/interfaces";
 import { v4 as uuidv4 } from "uuid";
 
 const filePath = path.join(__dirname, "..", "..", "data", "users.json");
 
-export function saveNewUserAccount(newUser: Omit<User, "id">): void {
-  // create a unique user id for each new user (temp solution change later)
-  const user: User = {
-    id: uuidv4(),
-    ...newUser,
-  };
-
+export async function saveNewUserAccount(newUser: Omit<User, "id">): Promise<void> {
   let users: User[] = [];
 
   // check if file path already exists
@@ -24,8 +19,21 @@ export function saveNewUserAccount(newUser: Omit<User, "id">): void {
     }
   }
 
+  // error checking: given email already has an account registered with it
+  if (users.find(u => u.email === newUser.email)) {
+    throw new Error("An account with this email already exists");
+  }
+
+  // create a unique user id for each new user (temp solution change later)
+  const modifiedNewUser: User = {
+    id: uuidv4(),
+    ...newUser,
+  };
+
+  // hash the user's password
+  modifiedNewUser.password = await bcrypt.hash(newUser.password, 10);
 
   // add the new user to the list of existing users
-  users.push(user);
+  users.push(modifiedNewUser);
   fs.writeFileSync(filePath, JSON.stringify(users, null, 2), "utf-8");
 }
