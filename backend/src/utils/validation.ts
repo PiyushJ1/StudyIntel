4ba3a814ff36c79f4b5107e39091385d9b67864c
Piyush <piyxsh@gmail.com>
2025-07-01 01:14:09 +1000

    create function to authenticate log in

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,3 +1,8 @@
+import fs from "fs";
+import path from "path";
+import bcrypt from "bcrypt";
+import { User, UserAuthResult } from "../models/interfaces";
+
 export function validateEmail(email: string): boolean {
   if (!email || typeof email !== "string") return false;
 
@@ -19,4 +24,28 @@ export function validatePassword(password: string): boolean {
   if (!passwordRegEx.test(password)) return false;
 
   return true;
+}
+
+export async function authenticateUser(email: string, password: string): Promise<UserAuthResult> {
+  // find user details from local JSON file
+  const filePath = path.join(__dirname, "..", "..", "data", "users.json");
+  const fileData = fs.readFileSync(filePath, "utf-8");
+
+  // create users array from the file's data
+  let users: User[] = [];
+  users = JSON.parse(fileData);
+
+  // no account exists associated to the email
+  const user = users.find((u) => u.email === email);
+  if (!user) {
+    return { result: false, message: "A user with this email does not exist" };
+  }
+
+  // check if entered password is correct
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return { result: false, message: "Incorrect password" };
+  }
+
+  return { result: true, message: "Log in succesful" };
 }
