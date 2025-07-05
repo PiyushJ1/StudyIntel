5fc1bd8578575a00b94fbd892fbc614da38f5c11
Piyush <piyxsh@gmail.com>
2025-07-06 02:06:08 +1000

    format for style

--- backend/src/utils/registerUserStorage.ts
+++ backend/src/utils/registerUserStorage.ts
@@ -4,14 +4,13 @@ import { v4 as uuidv4 } from "uuid";
 import db from "../db";
 
 export async function saveNewUserAccount(
-  newUser: Omit<User, "id">
+  newUser: Omit<User, "id">,
 ): Promise<void> {
   // select user with given email
-  const existingUser = await db.query(
-    "SELECT 1 FROM users WHERE email = $1",
-    [newUser.email]
-  );
-  
+  const existingUser = await db.query("SELECT 1 FROM users WHERE email = $1", [
+    newUser.email,
+  ]);
+
   // check if the email already has an account registered with it
   if (existingUser.rowCount && existingUser.rowCount > 0) {
     throw new Error("An account with this email has already been registered.");
@@ -22,6 +21,6 @@ export async function saveNewUserAccount(
 
   await db.query(
     "INSERT INTO users (id, firstName, lastName, email, password) VALUES ($1, $2, $3, $4, $5)",
-    [id, newUser.firstName, newUser.lastName, newUser.email, hashedPassword]
+    [id, newUser.firstName, newUser.lastName, newUser.email, hashedPassword],
   );
 }
