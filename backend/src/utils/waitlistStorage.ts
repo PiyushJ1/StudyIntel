342a322dd4366c45e2ebdb17ef5eaeaecf5a6d8a
Piyush <piyxsh@gmail.com>
2025-07-07 03:17:12 +1000

    remove debugging console logs and ensure that rowCount is truthy before eval

--- backend/src/utils/waitlistStorage.ts
+++ backend/src/utils/waitlistStorage.ts
@@ -1,19 +1,15 @@
 import db from "../db";
 
 export async function saveEmailToWaitlist(email: string): Promise<void> {
-  try {
-    // check if email is already in waitlist
-    const existingEmail = await db.query(
-      "SELECT 1 FROM waitlistemails WHERE emails = $1",
-      [email],
-    );
+  // check if email is already in waitlist
+  const existingEmail = await db.query(
+    "SELECT 1 FROM waitlistemails WHERE emails = $1",
+    [email],
+  );
 
-    if (existingEmail.rowCount === 0) {
-      await db.query("INSERT INTO waitlistemails (emails) VALUES ($1)", [email]);
-      console.log(`Email ${email} inserted successfully.`);
-    }
-  } catch (err) {
-    console.error("DB error in saveEmailToWaitlist:", err);
-    throw err;
-  } 
+  if (existingEmail.rowCount ?? 0 > 0) {
+    throw new Error("This email is already in the waitlist.");
+  }
+
+  await db.query("INSERT INTO waitlistemails (emails) VALUES ($1)", [email]);
 }
