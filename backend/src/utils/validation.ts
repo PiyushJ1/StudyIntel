124763e6cb896b161079ed0740f5abf7721d1f1a
Piyush <piyxsh@gmail.com>
2025-07-07 17:11:12 +1000

    add logic to authentice user when logging in

--- backend/src/utils/validation.ts
+++ backend/src/utils/validation.ts
@@ -1,6 +1,5 @@
-import fs from "fs";
-import path from "path";
 import bcrypt from "bcrypt";
+import db from "../db";
 import { User, UserAuthResult } from "../models/interfaces";
 
 export function validateEmail(email: string): boolean {
@@ -17,8 +16,12 @@ export function validatePassword(password: string): boolean {
   // no whitespace
   if (/\s/.test(password)) return false;
 
-  // regex to ensure password contains at least 9 chars, at least 1 uppercase,
-  // 1 lowercase, 1 digit, and 1 special character
+  // regex to ensure password contains at least:
+  // 9 chars, 
+  // 1 uppercase,
+  // 1 lowercase, 
+  // 1 digit, and 
+  // 1 special character
   const passwordRegEx =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\-\\/]).{9,}$/;
   if (!passwordRegEx.test(password)) return false;
@@ -30,26 +33,20 @@ export async function authenticateUser(
   email: string,
   password: string,
 ): Promise<UserAuthResult> {
-  // find user details from local JSON file
-  const filePath = path.join(__dirname, "..", "..", "data", "users.json");
-  const fileData = fs.readFileSync(filePath, "utf-8");
+  const result = await db.query("SELECT id, firstName, lastName, email, password FROM users WHERE email = $1", [
+    email,
+  ]);
 
-  // create users array from the file's data
-  let users: User[] = [];
-  users = JSON.parse(fileData);
-
-  // no account exists associated to the email
-  const user = users.find((u) => u.email === email);
-  if (!user) {
-    return { result: false, message: "A user with this email does not exist" };
+  if (result.rowCount === 0) {
+    return { result: false, message: "This email does not have an account registered" };
   }
 
-  // check if entered password is correct
+  const user = result.rows[0];
+
   const passwordMatch = await bcrypt.compare(password, user.password);
   if (!passwordMatch) {
     return { result: false, message: "Incorrect password" };
   }
 
-  // user log in auth successul
-  return { result: true, message: "Log in successful" };
+  return { result: true, message: "Login successful" };
 }
