6d100658cbe7040f263e640b47d850efd8af349a
Piyush <piyxsh@gmail.com>
2025-07-13 03:18:41 +1000

    use new error class for checking error instance

--- backend/src/routes/waitlistRoutes.ts
+++ backend/src/routes/waitlistRoutes.ts
@@ -1,6 +1,7 @@
 import validator from "validator";
 import { Router, Request, Response } from "express";
 import { saveEmailToWaitlist } from "../utils/waitlistStorage";
+import { EmailAlreadyInWaitlistError } from "../errors/auth";
 
 const router = Router();
 
@@ -8,17 +9,14 @@ router.post("/", async (req: Request, res: Response) => {
   const { email } = req.body;
 
   if (!validator.isEmail(email)) {
-    return res.status(400).json({ error: "Email provided was invalid" });
+    return res.status(400).json({ message: "Email provided was invalid" });
   }
 
   try {
     await saveEmailToWaitlist(email);
     return res.status(201).json({ message: "Waitlist joined successfully" });
   } catch (error) {
-    if (
-      error instanceof Error &&
-      error.message === "This email is already in the waitlist."
-    ) {
+    if (error instanceof EmailAlreadyInWaitlistError) {
       return res
         .status(409)
         .json({ message: "This email is already in the waitlist." });
