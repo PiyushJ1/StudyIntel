1a30d71cc5a6aeeefdb6d200770840abfdf0de0f
Piyush <piyxsh@gmail.com>
2025-07-19 03:25:10 +1000

    add debug logs

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -5,15 +5,20 @@ import { jwtVerify } from "jose";
 const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
 export async function middleware(req: NextRequest) {
+  console.log("hit");
   const token = req.cookies.get("token")?.value;
   if (!token) {
+      console.log("no token");
     return NextResponse.redirect(new URL("/login", req.url));
   }
-  
+    console.log("valid token");
+
   try {
     await jwtVerify(token, secret);
+      console.log("success");
     return NextResponse.next();
   } catch (error) {
+      console.log("err:", error);
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }
