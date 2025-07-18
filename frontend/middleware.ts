6145d94d155c9e632067e2b2c4adb2f941bcba07
Piyush <piyxsh@gmail.com>
2025-07-19 02:35:44 +1000

    use middleware to protect dashboard and its paths

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,25 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
-export function middleware(req: NextRequest) {
-  return NextResponse.next();
+const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
-  // retrieve cookie sent by the browser
+export async function middleware(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
-
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
-
-  // try {
-  //   jwt.verify(token, process.env.JWT_SECRET!);
-  //   return NextResponse.next();
-  // } catch (error) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  
+  try {
+    await jwtVerify(token, secret);
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 }
 
 export const config = {
-  matcher: ["/dashboard", "/dashboard/:path"],
+  matcher: ["/dashboard/:path*", "/dashboard"],
 }
+
