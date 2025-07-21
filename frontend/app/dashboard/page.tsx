424184065b8a4710482fbbc8eae0a62aada3b272
Piyush <piyxsh@gmail.com>
2025-07-22 00:54:08 +1000

    style: fix unnecessary white space

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -41,7 +41,6 @@ export default function DashboardPage() {
               animateOn="view"
             />
             {" "}
-              
             {loading ? (
               <span className={styles.highlightUser}></span>
             ) : (
