a454579beaa4ddfa8d01beb7c7288bd9e8fa78c0
Piyush <piyxsh@gmail.com>
2025-07-30 02:13:56 +1000

    use session start and finish routers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import waitlistRoutes from "./routes/waitlistRoutes";
-import signupUserRoutes from "./routes/signupUserRoutes";
+import signupUserRoute from "./routes/signupUserRoute";
 import signinRoute from "./routes/signinRoute";
+import startSession from "./routes/startSessionRoute";
+import finishSession from "./routes/finishSessionRoute";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -23,10 +25,16 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/waitlist", waitlistRoutes);
 
 // signup new user and save their info
-app.use("/api/signup", signupUserRoutes);
+app.use("/api/signup", signupUserRoute);
 
 // authentice user log in
 app.use("/api/signin", signinRoute);
+
+// start new study session
+app.use("/api/start-session", startSession);
+
+// finish and save study session info
+app.use("/api/finish-session", finishSession);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
