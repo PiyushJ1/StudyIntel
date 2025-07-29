b56875cb6dd2b687384b167165f32f0991c72425
Piyush <piyxsh@gmail.com>
2025-07-30 02:21:19 +1000

    create functions to handle start and finish sessions (placeholder for now)

--- frontend/components/NewSessionPopup.tsx
+++ frontend/components/NewSessionPopup.tsx
@@ -19,6 +19,49 @@ export default function NewSessionPopup({ isOpen, onClose, seconds, _running, se
     }
   };
 
+  const handleStartSession = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log("Calling session start endpoint");
+
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/start-session`, {
+        method: "POST",
+        credentials: "include",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify("session here")
+      });
+
+      if (!response.ok) {
+        // error handling
+      }
+    } catch (err) {
+      alert("Could not save session start info");
+      console.log("Error:", err);
+    }
+  }
+
+  const handleFinishSession = async (e: React.MouseEvent) => {
+    e.preventDefault();
+    console.log("Calling session finish endpoint");
+
+    // save study session info to db
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users/finish-session`, {
+        method: "PATCH",
+        credentials: "include",
+        headers: { "Content-Type": "application/json"},
+        body: JSON.stringify("session here")
+      });
+
+      if (!response.ok) {
+        // error handling
+      }
+    } catch (err) {
+      alert("Could not save session end info");
+      console.log("Error:", err);
+    }
+  }
+
   const formatTime = (s: number) => {
     const hours = String(Math.floor(s / 3600)).padStart(2, '0');
     const mins = String(Math.floor((s % 3600) / 60)).padStart(2, '0');
@@ -51,6 +94,16 @@ export default function NewSessionPopup({ isOpen, onClose, seconds, _running, se
             <button className={styles.startButton} onClick={() => setRunning(true)}>Start</button>
             <button className={styles.pauseButton} onClick={() => setRunning(false)}>Pause</button>
             <button className={styles.resetButton} onClick={() => (setSeconds(0), setRunning(false))}>Reset</button>
+            <button 
+              className={styles.finishButton} 
+              onClick={async (e) => {
+                handleFinishSession(e);
+                setSeconds(0);
+                setRunning(false);
+              }}
+            >
+              Finish
+            </button>
           </div>
         </>
       </div>
