2f1ecff9986d2a89b49af305e147b52c2025dd49
Piyush <piyxsh@gmail.com>
2025-07-30 01:07:11 +1000

    create placeholder files for study session start and end routes

--- backend/src/routes/finishSessionRoute.ts
+++ backend/src/routes/finishSessionRoute.ts
@@ -3,13 +3,10 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  const { userId, endTime, status} = req.body;
+  const { userId, endTime, status } = req.body;
 
   try {
-    
-  } catch (err) {
-
-  }
+  } catch (err) {}
 });
 
 export default router;
