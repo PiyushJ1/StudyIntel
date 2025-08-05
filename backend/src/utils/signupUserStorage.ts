4b61d20df06249153887d232c77fc946b00311e6
Piyush <piyxsh@gmail.com>
2025-08-05 19:14:07 +1000

    migrate raw SQL to Prisma schema

--- backend/src/utils/signupUserStorage.ts
+++ backend/src/utils/signupUserStorage.ts
@@ -1,26 +1,32 @@
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { User } from "../models/interfaces";
 import { v4 as uuidv4 } from "uuid";
-import db from "../db";
+
+const prisma = new PrismaClient();
 
 export async function saveNewUserAccount(
   newUser: Omit<User, "id">,
 ): Promise<void> {
-  // select user with given email
-  const existingUser = await db.query("SELECT 1 FROM users WHERE email = $1", [
-    newUser.email,
-  ]);
+  const existingUser = await prisma.user.findUnique({
+    where: { email: newUser.email },
+  })
 
   // check if the email already has an account signed up with it
-  if (existingUser.rowCount ?? 0 > 0) {
+  if (existingUser) {
     throw new Error("An account with this email has already been signed up.");
   }
 
   const hashedPassword = await bcrypt.hash(newUser.password, 10);
   const id = uuidv4();
 
-  await db.query(
-    "INSERT INTO users (id, firstName, lastName, email, password) VALUES ($1, $2, $3, $4, $5)",
-    [id, newUser.firstName, newUser.lastName, newUser.email, hashedPassword],
-  );
+  await prisma.user.create({
+    data: {
+      id: id,
+      firstname: newUser.firstName,
+      lastname: newUser.lastName,
+      email: newUser.email,
+      password: hashedPassword,
+    }
+  });
 }
