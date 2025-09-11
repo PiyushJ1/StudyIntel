0a8215889ac1730ac80449be075900eec446b898
Piyush <piyxsh@gmail.com>
2025-09-12 00:15:27 +1000

    feat: call perplexity API to web scrape the course's topics in week by week format

---
+++ backend/src/utils/pplxApiCalls.ts
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function getCourseTopics(courseCode: string): Promise<any> {
+  const url = "https://api.perplexity.ai/chat/completions";
+  const headers = {
+    Authorization: `Bearer ${process.env.PPLX_API_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  const payload = {
+    model: "sonar-pro",
+    messages: [
+      {
+        role: "user",
+        content: `Provide a week-by-week list of main lecture topics for the UNSW course ${courseCode} 
+          for 2025 Term 2 from Weeks 1-10. Since Week 6 is flex week don't mention any topic and just say 
+          "Flex Week". Output exactly in this format with Week [number]: [topic] one per line, 
+          no extra explanation or formatting. Return the output in JSON format`,
+      },
+    ],
+  };
+
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(payload),
+    });
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error calling Perplexity API:", error);
+    throw new Error("Failed to fetch topics from Pplx API");
+  }
+}
