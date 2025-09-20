eabdbe229327e7e69ea78b692f3e129ebed9b38c
PiyushJ1 <piyxsh@gmail.com>
2025-09-21 01:24:21 +1000

    feat: protect profile page

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -19,6 +19,6 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard", "/courses", "/insights"],
+  matcher: ["/dashboard", "/courses", "/insights", "/profile"],
 }
 
