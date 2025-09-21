d357d5c42cd695598d22ff15c0a2fe8cfb79db2e
PiyushJ1 <piyxsh@gmail.com>
2025-09-22 00:35:58 +1000

    feat: patch and finish study session and store to db

--- backend/src/routes/finishSessionRoute.ts
+++ backend/src/routes/finishSessionRoute.ts
@@ -1,12 +1,29 @@
 import { Router, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
-router.post("/", async (req: Request, res: Response) => {
-  const { userId, endTime, status } = req.body;
-
+router.patch("/", async (req: Request, res: Response) => {
   try {
-  } catch (err) {}
+    const { sessionId } = req.body;
+
+    if (!sessionId) {
+      return res
+        .status(400)
+        .json({ error: "sessionId is required to end the session " });
+    }
+
+    const updatedSession = await prisma.studySession.update({
+      where: { id: sessionId },
+      data: { endTime: new Date() },
+    });
+
+    return res.status(200).json({ sessionId: updatedSession.id });
+  } catch (err) {
+    console.error("Error finishing and saving study session", err);
+    return res.status(500).json({ error: "Could not finish study session" });
+  }
 });
 
 export default router;
