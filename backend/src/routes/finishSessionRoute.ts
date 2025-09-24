e810a800801ed5db84b673739cc22ce906e3272b
PiyushJ1 <piyxsh@gmail.com>
2025-09-24 17:42:16 +1000

    feat: store the session duration when finsihing session

--- backend/src/routes/finishSessionRoute.ts
+++ backend/src/routes/finishSessionRoute.ts
@@ -14,9 +14,25 @@ router.patch("/", async (req: Request, res: Response) => {
         .json({ error: "sessionId is required to end the session " });
     }
 
+    const session = await prisma.studySession.findUnique({
+      where: { id: sessionId },
+    });
+
+    if (!session) {
+      return res.status(404).json({ error: "The session does not exist" });
+    }
+
+    const currentTime = new Date();
+    const sessionLength = Math.floor(
+      (currentTime.getTime() - session.startTime.getTime()) / 1000, // in seconds
+    );
+
     const updatedSession = await prisma.studySession.update({
       where: { id: sessionId },
-      data: { endTime: new Date() },
+      data: {
+        endTime: currentTime,
+        duration: sessionLength,
+      },
     });
 
     return res.status(200).json({ sessionId: updatedSession.id });
