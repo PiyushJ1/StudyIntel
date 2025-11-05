82a6a0479f1023e2438aa42ff74d88042aef2aea
PiyushJ1 <piyxsh@gmail.com>
2025-11-05 22:33:05 +1100

    feat: implement caching with redis

--- frontend/app/api/me/route.ts
+++ frontend/app/api/me/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
+import redis from "../../../../backend/src/lib/redis";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
@@ -20,6 +21,21 @@ export async function GET(_req: Request) {
     const { payload } = await jwtVerify(token, secret);
     const userId = payload.userId;
 
+    let cachedFirstName = await redis.get(`user:${userId}:firstname`);
+    let cachedCourses = await redis.get(`user:${userId}:courses`);
+    let cachedTimeStudied = await redis.get(`user:${userId}:totalStudyTimes`);
+
+    // short circuit code to return cached fields (cache hit)
+    if (cachedFirstName && cachedCourses && cachedTimeStudied) {
+      return NextResponse.json({
+        userId,
+        firstname: cachedFirstName,
+        timestudied: cachedTimeStudied,
+        courses: cachedCourses,
+      })
+    }
+
+    // cache miss (couldn't find cache for items)
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/users/${userId}`,
       {
@@ -31,6 +47,7 @@ export async function GET(_req: Request) {
       },
     );
 
+    // failed response
     if (!res.ok) {
       return NextResponse.json(
         { error: "Failed to fetch user info " },
@@ -38,13 +55,35 @@ export async function GET(_req: Request) {
       );
     }
 
+    // manually set fields from response
     const data = await res.json();
+    const firstName = data.user.firstname;
+    const totalStudyTimes = data.totalStudyTimes;
+    const courses = data.user.courses;
+
+    // cache the fields if they haven't been already
+    if (!cachedFirstName && firstName) {
+      await redis.set(`user:${userId}:firstname`, firstName, { ex: 100000 });
+      cachedFirstName = firstName;
+    }
+    if (!cachedTimeStudied && totalStudyTimes) {
+      await redis.set(
+        `user:${userId}:totalStudyTimes`,
+        JSON.stringify(totalStudyTimes),
+        { ex: 100000 }
+      );
+      cachedTimeStudied = totalStudyTimes;
+    }
+    if (!cachedCourses && courses) {
+      await redis.set(`user:${userId}:courses`, courses, { ex: 10000 });
+      cachedCourses = courses;
+    }
 
     return NextResponse.json({
       userId,
-      firstname: data.user.firstname,
-      timestudied: data.totalStudyTimes,
-      courses: data.user.courses,
+      firstname: firstName,
+      timestudied: totalStudyTimes,
+      courses: courses,
     });
   } catch (err) {
     console.error("Err in fetching user info: ", err);
