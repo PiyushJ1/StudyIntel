412de92e2246086de8a31641243e7de0a873f56d
PiyushJ1 <piyxsh@gmail.com>
2025-11-13 16:12:05 +1100

    refactor: caching logic into backend

--- frontend/app/api/me/route.ts
+++ frontend/app/api/me/route.ts
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
-import redis from "../../../../backend/src/lib/redis";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
 
-export async function GET(_req: Request) {
+export async function GET() {
   // retrieve cookie token from html
   const cookieStore = cookies();
   const token = (await cookieStore).get("token")?.value;
@@ -21,21 +20,6 @@ export async function GET(_req: Request) {
     const { payload } = await jwtVerify(token, secret);
     const userId = payload.userId;
 
-    let cachedFirstName = await redis.get(`user:${userId}:firstname`);
-    let cachedCourses = await redis.get(`user:${userId}:courses`);
-    let cachedTimeStudied = await redis.get(`user:${userId}:totalStudyTimes`);
-
-    // short circuit code to return cached fields (cache hit)
-    if (cachedFirstName && cachedCourses && cachedTimeStudied) {
-      return NextResponse.json({
-        userId,
-        firstname: cachedFirstName,
-        timestudied: cachedTimeStudied,
-        courses: cachedCourses,
-      });
-    }
-
-    // cache miss (couldn't find cache for items)
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/users/${userId}`,
       {
@@ -58,26 +42,8 @@ export async function GET(_req: Request) {
     // manually set fields from response
     const data = await res.json();
     const firstName = data.user.firstname;
-    const totalStudyTimes = data.totalStudyTimes;
+    const totalStudyTimes = data.timestudied;
     const courses = data.user.courses;
-
-    // cache the fields if they haven't been already
-    if (!cachedFirstName && firstName) {
-      await redis.set(`user:${userId}:firstname`, firstName, { ex: 100000 });
-      cachedFirstName = firstName;
-    }
-    if (!cachedTimeStudied && totalStudyTimes) {
-      await redis.set(
-        `user:${userId}:totalStudyTimes`,
-        JSON.stringify(totalStudyTimes),
-        { ex: 100000 },
-      );
-      cachedTimeStudied = totalStudyTimes;
-    }
-    if (!cachedCourses && courses) {
-      await redis.set(`user:${userId}:courses`, courses, { ex: 10000 });
-      cachedCourses = courses;
-    }
 
     return NextResponse.json({
       userId,
