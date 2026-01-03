85e701365b6cc2341eeec4548fd4e1dc42fc4852
PiyushJ1 <piyxsh@gmail.com>
2026-01-04 01:00:33 +1100

    fix: use prisma v6 adapter config

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import "dotenv/config";
-// import { PrismaClient } from "../../prisma/generated/client.js";
 import { PrismaClient } from "@prisma/generated/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000, // increased timeout for cloud DBs
-  ssl: {
-    rejectUnauthorized: false, // required for Railway and similar cloud providers
-  },
+  connectionTimeoutMillis: 5_000,
+  idleTimeoutMillis: 300_000,
 });
 
 const prisma = new PrismaClient({ adapter });
