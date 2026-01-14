3d5112b171a822adad7f72fcffd47049a53445a1
PiyushJ1 <piyxsh@gmail.com>
2026-01-14 22:03:45 +1100

    feat: create new route to get all study sessions for a user

---
+++ backend/src/routes/pastSessions.ts
@@ -0,0 +1,28 @@
+import { Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma";
+
+const router = Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const token = req.cookies.token;
+
+    // validate token
+    if (!token) {
+      return res.status(401).json({ error: "Not Authenticated" });
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+    };
+
+    let sessions = await prisma.studySession.findMany({
+      where: { userId: payload.userId },
+    });
+
+    console.log(sessions);
+  } catch (err) {
+    console.log("error: ", err);
+  }
+});
